ce8a07541fa30c42119dd79bb226be4adc2ffc99
Rafcksdev <rafcksps@gmail.com>
2025-04-18 13:41:31 -0300

    Feat: static components and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BigCardComponent } from "./components/big-card/big-card.component";
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { TitleComponent } from './components/title/title.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BigCardComponent, MenuBarComponent, TitleComponent, SmallCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-blog';
-}
+export class AppComponent{}
